685e31291866958b12994e20b84093aee18fbabb
Elias Assuncao <elias3748@gmail.com>
2019-09-05 21:15:29 -0300

    Adicionando documentação da API

--- SchedulingAPI/Scheduling.Infra/DataContexts/SchedulingDataContext.cs
+++ SchedulingAPI/Scheduling.Infra/DataContexts/SchedulingDataContext.cs
@@ -39,14 +39,11 @@ namespace Scheduling.Infra.DataContexts
             context.Rooms.Add(new Rooms { Id = 2, Title = "Sala 2", Room = 16, Status = 0 });
             context.Rooms.Add(new Rooms { Id = 3, Title = "Sala 3", Room = 34, Status = 1 });
             context.Rooms.Add(new Rooms { Id = 4, Title = "Sala 4", Room = 35, Status = 1 });
-            context.Rooms.Add(new Rooms { Id = 5, Title = "Sala 5", Room = 42, Status = 0 });
-            context.Rooms.Add(new Rooms { Id = 6, Title = "Sala 6", Room = 50, Status = 1 });
-            context.Rooms.Add(new Rooms { Id = 7, Title = "Sala 7", Room = 51, Status = 1 });
             context.SaveChanges();
 
             //agendamentos criados
             context.Schedules.Add(new Schedules { Id = 1, RoomsId = 2, Created = DateTime.Now });
-            context.Schedules.Add(new Schedules { Id = 2, RoomsId = 5, Created = DateTime.Now });
+            context.Schedules.Add(new Schedules { Id = 2, RoomsId = 4, Created = DateTime.Now });
             context.SaveChanges();
 
             base.Seed(context);
